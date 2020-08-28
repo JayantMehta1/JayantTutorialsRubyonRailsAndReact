bc0f4d7a8ee1994facc547c3856a18cb7e03c53a
Jayant Mehta <jay9mehta@gmail.com>
2020-08-28 17:31:46 -0400

    process styling

--- app/javascript/components/Home/Table/Thumbnail.js
+++ app/javascript/components/Home/Table/Thumbnail.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 const Thumbnail = (props) => {
     return (
         <div className="pt-4 pb-4">
-            <iframe class="episodeThumbnail" src={props.url} allow="accelerometer; autoplay; encypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe class="episodeFrames" width="100%" height="100%" src={props.url} frameBorder="0" allow="accelerometer; autoplay; encypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     )
 }
